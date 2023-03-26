9aade616f13cf01aa0b4c2a3f6db31cafcc3250c
Yarl <yarl_48ru@mail.ru>
2023-03-26 11:48:22 +0300

    Добавление примитивной клавиатуры

--- data.py
+++ data.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = '6238823064:AAHqSFDUaFbJ6WXBI1bNay5HZdSOjeFJRQw'
 COMPANY_NAME = "TESTMED"
+DOCTORS = ['Саша', 'Петя', 'Вася', 'Серый', 'Черный']
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Application, MessageHandler, CommandHandler, filters
 
-from data import BOT_TOKEN, COMPANY_NAME
+from data import BOT_TOKEN, COMPANY_NAME, DOCTORS
 
 
 logging.basicConfig(
@@ -15,24 +15,38 @@ logger = logging.getLogger(__name__)
 
 kb_syms = [['/start']]
 kb = ReplyKeyboardMarkup(kb_syms, one_time_keyboard=True)
+doctors_lst = []
+for i in range(1, len(DOCTORS) + 1):
+    if i % 2 != 0:
+        doctors_lst.append([DOCTORS[i - 1]])
+    else:
+        doctors_lst[-1].append(DOCTORS[i - 1])
+doctors_lst[-1].append('/cancel') if len(doctors_lst[-1]) == 1 else doctors_lst.append(['/cancel'])
+dctr = ReplyKeyboardMarkup(doctors_lst, one_time_keyboard=True)
 
 
 async def misunderstanding(update, context):
-    await update.message.reply_text('Что-то я не разобрался... Напишите "/start" для начала работы.', reply_markup=kb)
+    await update.message.reply_text('Что-то я не разобрался... \nНапишите "/start" для начала работы.', reply_markup=kb)
 
 
 async def begining(update, context):
     await update.message.reply_html(f"""\tЗдравствуйте! Я - бот для записи на прием в компании {COMPANY_NAME}
-    Выберите один из вариантов, предоставленных ниже.""")
+    \nВыберите один из вариантов, предоставленных ниже.""", reply_markup=dctr)
+
+
+async def cancellation(update, context):
+    await update.message.reply_text('До скорой встречи! \nНапишите "/start" для возобновления работы.', reply_markup=kb)
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     text_handler = MessageHandler(filters=filters.TEXT & ~filters.COMMAND, callback=misunderstanding)
     start_handler = CommandHandler(command='start', callback=begining)
+    end_handler = CommandHandler(command='cancel', callback=cancellation)
 
     application.add_handler(text_handler)
     application.add_handler(start_handler)
+    application.add_handler(end_handler)
 
     application.run_polling()
 
